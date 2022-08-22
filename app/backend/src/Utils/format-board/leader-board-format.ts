8e7add5c19306d467791d7693b70f2b09267a750
nonyil <igbrendow@gmail.com>
2022-08-22 16:16:58 -0300

    arrumando os sinais da 29

--- app/backend/src/Utils/format-board/leader-board-format.ts
+++ app/backend/src/Utils/format-board/leader-board-format.ts
@@ -18,12 +18,12 @@ export default function leaderBoardFormat(boardTeams: ILeaderBoardTeam[]) {
     let totalPoints = 0; let totalGames = 0; let totalVictories = 0; let totalDraws = 0;
     let totalLosses = 0; let goalsFavor = 0; let goalsOwn = 0; let goalsBalance = 0;
     team.homeMatches.forEach((a) => {
-      if (a.homeTeamGoals < a.awayTeamGoals) { totalVictories += 1; } // +vitorias
+      if (a.homeTeamGoals > a.awayTeamGoals) { totalVictories += 1; } // +vitorias
       if (a.homeTeamGoals === a.awayTeamGoals) { totalDraws += 1; } // +empates
-      if (a.homeTeamGoals > a.awayTeamGoals) { totalLosses += 1; } // +derrotas
-      if (a.homeTeamGoals < a.awayTeamGoals) { totalPoints += 3; } // + pontos vitorias
+      if (a.homeTeamGoals < a.awayTeamGoals) { totalLosses += 1; } // +derrotas
+      if (a.homeTeamGoals > a.awayTeamGoals) { totalPoints += 3; } // + pontos vitorias
       if (a.homeTeamGoals === a.awayTeamGoals) { totalPoints += 1; } // + pontos empate
-      totalGames += 1; goalsFavor += a.awayTeamGoals; goalsOwn += a.homeTeamGoals;
+      totalGames += 1; goalsFavor += a.homeTeamGoals; goalsOwn += a.awayTeamGoals;
     });
     goalsBalance = goalsFavor - goalsOwn;
     const efficiency = parseFloat(((totalPoints / (totalGames * 3)) * 100).toFixed(2));
